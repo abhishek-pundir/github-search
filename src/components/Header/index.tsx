a95b9b131e997ab9c48f75a4f5c2174a2a7bcc48
Abhishek Pundir <abhishekpundir@Abhisheks-MacBook-Pro.local>
2023-03-13 00:47:22 +0530

    feat: Set up initial TypeScript configuration for React project

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -15,10 +15,10 @@ const Header = () => {
     setIsRoot(isRootLocation);
 
     const query = new URLSearchParams(location.search).get("q");
-    setQuery(query);
+    setQuery(query ?? "");
   }, [location]);
 
-  function handleLogoClick(event) {
+  function handleLogoClick(event: React.MouseEvent<HTMLAnchorElement>) {
     event.preventDefault();
     navigate("/");
   }
